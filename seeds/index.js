3912ecb02af82445cba2b12790d26e424eb2ae2b
TheRajath <48343927+TheRajath@users.noreply.github.com>
2023-03-25 14:37:29 +0530

    Campgrounds crud operations (#1)
    * initial home page
    
    * mongoose connection setup
    
    * Seed random data
    
    * show all campgrounds
    
    * show campground byId
    
    * Add new campground
    
    * update campground
    
    * delete campground
    
    * Create pr-checks.yml
    
    * Update pr-checks.yml

---
+++ seeds/index.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+
+mongoose.connect('mongodb://127.0.0.1:27017/camp-grounds');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDb = async () => {
+
+    await Campground.deleteMany({});
+
+    for (let i = 0; i < 50; i++) {
+
+        const random1000 = Math.floor(Math.random() * 1000);
+
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+
+        await camp.save();
+    }
+}
+
+seedDb().then(() => {
+    mongoose.connection.close();
+});
