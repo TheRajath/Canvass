394814832409d96f7bfdfa9c654dcabc94fec4a2
TheRajath <sairajath7@gmail.com>
2023-03-25 13:36:09 +0530

    show all campgrounds

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/campgrounds', async (req, res) => {
+
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+});
+
 app.listen(3000, () => {
 
     console.log('Serving on port 3000');
