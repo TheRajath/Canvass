5752455201b79e9284f6b7d8c356f42f03a25577
TheRajath <sairajath7@gmail.com>
2023-03-31 08:55:05 +0530

    register a new user

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const catchAsync = require('../utils/catchAsync');
 
 router.get('/register', (req, res) => {
 
     res.render('users/register');
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', catchAsync(async (req, res) => {
 
-    res.send(req.body);
-});
+    try {
+
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        
+        req.flash('success', 'Welcome to Yelp Camp!');
+        res.redirect('/campgrounds');
+
+    } catch (e) {
+
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}));
 
 module.exports = router;
