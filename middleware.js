79579a5aafe59107d90f3545a2c8e43bcdf25388
TheRajath <sairajath7@gmail.com>
2023-04-01 09:55:18 +0530

    isLoggedIn middleware

---
+++ middleware.js
@@ -0,0 +1,10 @@
+module.exports.isLoggedIn = (req, res, next) => {
+
+    if (!req.isAuthenticated()) {
+
+        req.flash('error', 'You must be signed in first!');
+        res.redirect('/login');
+    }
+
+    next();
+}
