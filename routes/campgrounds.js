1bc241e3aecdb6f29f032818a46936d76c15dea3
TheRajath <48343927+TheRajath@users.noreply.github.com>
2023-04-01 13:33:43 +0530

    Add authors campground (#7)
    * associate campground with an author
    
    * mode all validations to middleware.js
    
    * associate reviews with users
    
    * authorize review delete button

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,26 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { campgroundSchema } = require('../schemas.js');
-const { isLoggedIn } = require('../middleware');
-
-const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
-
-const validateCampground = (req, res, next) => {
-
-    const { error } = campgroundSchema.validate(req.body);
-
-    if (error) {
-
-        const message = error.details.map(el => el.message).join(',');
-        throw new ExpressError(message, 400);
-
-    } else {
-
-        next();
-    }
-};
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
 router.get('/', catchAsync(async (req, res) => {
 
@@ -36,6 +18,7 @@ router.get('/new', isLoggedIn, (req, res) => {
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
 
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
 
     req.flash('success', 'Successfully made a new campground');
@@ -45,7 +28,13 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res) => 
 
 router.get('/:id', catchAsync(async (req, res) => {
 
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id)
+        .populate({
+            path: 'reviews',
+            populate: {
+                path: 'author'
+            }
+        }).populate('author');
 
     if (!campground) {
 
@@ -55,9 +44,10 @@ router.get('/:id', catchAsync(async (req, res) => {
     res.render('campgrounds/show', { campground });
 }));
 
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
 
-    const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
 
     if (!campground) {
 
@@ -68,7 +58,7 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     res.render('campgrounds/edit', { campground });
 }));
 
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
 
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
@@ -78,7 +68,7 @@ router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) =
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
 
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
--- seeds/index.js
+++ seeds/index.js
@@ -23,6 +23,7 @@ const seedDb = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
 
         const camp = new Campground({
+            author: '6427c9999ba7e78116bf0c06',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
