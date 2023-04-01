ca92950c5364b15a3496ab79a9de4b9d50cb657e
TheRajath <sairajath7@gmail.com>
2023-04-01 10:13:39 +0530

    logout user

--- routes/users.js
+++ routes/users.js
@@ -37,4 +37,14 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     req.flash('success', 'welcome back!');
 });
 
+router.get('/logout', (req, res, next) => {
+
+    req.logout(err => {
+        if (err) return next(err);
+
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+});
+
 module.exports = router;
