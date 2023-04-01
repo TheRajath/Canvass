a64d007487092a4a513c941f2fa6a63c3da9b239
TheRajath <sairajath7@gmail.com>
2023-04-01 13:07:51 +0530

    associate reviews with users

--- routes/reviews.js
+++ routes/reviews.js
@@ -5,14 +5,13 @@ const catchAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 
-
-
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
 
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
 
     campground.reviews.push(review);
 
