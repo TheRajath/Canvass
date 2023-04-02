90f0e234593534d362a19a852d59fa249c9c52e0
TheRajath <sairajath7@gmail.com>
2023-04-02 12:15:54 +0530

    group routes with same resource

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,17 @@ const catchAsync = require('../utils/catchAsync');
 const passport = require('passport');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
-
-router.get('/login', users.renderLogin);
-
-router.post('/login', passport.authenticate('local', {
-    failureFlash: true,
-    failureRedirect: '/login',
-    keepSessionInfo: true
-}), users.login);
+router.route('/login')
+    .get(users.renderLogin)
+    .post(passport.authenticate('local', {
+        failureFlash: true,
+        failureRedirect: '/login',
+        keepSessionInfo: true
+    }), users.login);
 
 router.get('/logout', users.logout);
 
