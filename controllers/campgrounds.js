a34a104baa5b34ece79a3690677672ddb29e9fc4
TheRajath <48343927+TheRajath@users.noreply.github.com>
2023-04-11 16:55:00 +0530

    Apply suggestions from code review

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -62,7 +62,6 @@ module.exports.updateCampground = async (req, res) => {
     const images = req.files.map(f => ({ url: f.path, filename: f.filename }))
 
     campground.images.push(...images);
-
     await campground.save();
 
     req.flash('success', 'Successfully updated campground!');
