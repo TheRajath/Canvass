2f463ce65defaf1dd0a3a73d12f5f2db36b23688
Sai Rajath <sairajath7@gmail.com>
2023-04-14 11:08:17 +0530

    Set secret

--- app.js
+++ app.js
@@ -46,9 +46,11 @@ app.use(mongoSanitize({
     replaceWith: '_'
 }));
 
+secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
 const store = new MongoDBStore({
     mongoUrl: dbUrl,
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     touchAfter: 24 * 60 * 60
 });
 
@@ -60,7 +62,7 @@ store.on("error", function (e) {
 const sessionConfig = {
     store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
